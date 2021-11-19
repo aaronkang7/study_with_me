2644142450d8f36bf2052cfc28d4c2f13bc45308
Aaron Kang <aaronkang612@gmail.com>
2021-11-19 00:20:03 -0500

    reset ios

---
+++ ios-frontend/ios-frontend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-frontend
+//
+//  Created by Aaron Kang on 11/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
